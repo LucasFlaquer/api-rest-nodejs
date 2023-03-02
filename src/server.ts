74f65c1b8e54d22134f6ca89816ec873dfa17171
LucasFlaquer <lucas.flaquer@gmail.com>
2023-03-02 16:16:43 -0300

    setup database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', (req, res) => {
-  return 'Hello World!'
+app.get('/hello', async (req, res) => {
+  const test = await knex('sqlite_schema').select('*')
+
+  return test
 })
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP SERVER RUNNING'))
