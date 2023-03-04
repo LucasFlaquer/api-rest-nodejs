a9728a7498fa419f6488c49bd9920bb1a438adcc
LucasFlaquer <lucas.flaquer@gmail.com>
2023-03-04 15:43:33 -0300

    env and migrations

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import { knex as setupKnex, Knex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set')
+}
+
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async (req, res) => {
-  const test = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions').select('*')
+  // .insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Transação de teste',
+  //   amount: 100,
+  // })
+  // .returning('*')
 
-  return test
+  return transaction
 })
 
 app.listen({ port: 3333 }).then(() => console.log('HTTP SERVER RUNNING'))
